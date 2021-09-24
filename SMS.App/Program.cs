30c95051b3c5d85d88d32be6c3b3cd2ff2351916
golam habib palash <golamhabibpalash@hotmail.com>
2021-09-24 23:03:12 +0600

    Logger Configuration Stablished in Program.cs file

--- SMS.App/Program.cs
+++ SMS.App/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,11 @@ namespace SchoolManagementSystem
     {
         public static void Main(string[] args)
         {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true).Build();
+
+            Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -20,7 +26,7 @@ namespace SchoolManagementSystem
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseSerilog();
                 });
     }
 }

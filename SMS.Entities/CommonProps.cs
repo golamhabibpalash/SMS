2b9e7612561826fced7109b3755824ddb91e5a93
golam habib palash <golamhabibpalash@hotmail.com>
2021-09-24 20:32:06 +0600

    CommonProps add in Model Entities for all model class

---
+++ SMS.Entities/CommonProps.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Entities
+{
+    public class CommonProps
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+        [Display(Name = "Created At")]
+        public DateTime CreatedAt { get; set; }
+
+        [Display(Name = "Edited By")]
+        public string EditedBy { get; set; }
+
+        [Display(Name = "Edited At")]
+        public DateTime EditedAt { get; set; }
+    }
+}

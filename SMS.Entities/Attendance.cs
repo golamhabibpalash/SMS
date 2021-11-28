27213772ffaefa4692547bdca70ea5c3962d1d73
golam habib palash <golamhabibpalash@hotmail.com>
2021-11-28 07:04:29 +0600

    FingerPrint model added and Attendance added

--- SMS.Entities/Attendance.cs
+++ SMS.Entities/Attendance.cs
@@ -9,11 +9,12 @@ namespace SMS.Entities
     public class Attendance
     {
         public int Id { get; set; }
+
         public DateTime AttendanceDate { get; set; }
+
+        [Display(Name ="Attendance")]
         public bool IsPresent { get; set; }
-        public int StudentId { get; set; }
-        public Student Student { get; set; }
-        
+
         [DataType(DataType.Time)]
         [DisplayFormat(DataFormatString ="{0:hh:mm:ss}")]
         public DateTime InTime { get; set; }
@@ -22,12 +23,8 @@ namespace SMS.Entities
         [DisplayFormat(DataFormatString = "{0:hh:mm:ss}")]
         public DateTime OutTime { get; set; }
 
-        [Display(Name = "Class")]
-        public int AcademicClassId { get; set; }
-        public AcademicClass AcademicClass { get; set; }
-
-        [Display(Name ="Session")]
-        public int AcademicSessionId { get; set; }
-        public AcademicSession AcademicSession { get; set; }
+        [Display(Name ="User")]
+        public int UserId { get; set; }
+        public char UserType { get; set; }
     }
 }

3aa8053b4067f8e3d3e04638ad5f4383ee19d30c
golam habib palash <golamhabibpalash@gmail.com>
2021-12-13 13:05:57 +0600

    Profile Overlay modified

---
+++ SMS.App/Utilities/ViewComponents/ProfileOverlayViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMS.App.Utilities.ViewComponents
+{
+    public class ProfileOverlayViewComponent : ViewComponent
+    {
+        public  IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
--- SMS.App/wwwroot/js/Accessories/userProfileOverLay.js
+++ SMS.App/wwwroot/js/Accessories/userProfileOverLay.js
@@ -1,8 +1,27 @@
 ﻿
-    function openNav() {
-        document.getElementById("myNav").style.width = "25%";
+var width = Math.max(window.screen.width, window.innerWidth);
+function openNav() {
+
+
+    if (width < 450) {
+        document.getElementById("myNav").style.width = "82%";
+    }
+    else {
+        document.getElementById("myNav").style.width = "15%";
+    }
+    
+       
 }
 
-    function closeNav() {
-        document.getElementById("myNav").style.width = "0%";
+function closeNav() {
+        document.getElementById("myNav").style.width = "0%"; 
 }
+
+
+
+    //document.querySelectorAll('*')
+    //    .forEach(element => element.addEventListener('click', e => {            
+    //        if (divWidth > 0) {
+    //            console.log(divWidth);
+    //        }
+    //    }))

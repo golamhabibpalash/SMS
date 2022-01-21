03ef6c91a03114ceab848cc166213566f3c2eb5b
GH Palash <golamhabibpalash@gmail.com>
2022-01-22 02:30:59 +0600

    AttendanceMachine Controller added with index view scaffold only

---
+++ SMS.App/Controllers/AttendanceMachinesController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMS.BLL.Contracts;
+using System.Threading.Tasks;
+
+namespace SMS.App.Controllers
+{
+    public class AttendanceMachinesController : Controller
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly IAttendanceMachineManager _attendanceMachineManager;
+        public AttendanceMachinesController(IHttpContextAccessor contextAccessor, IAttendanceMachineManager attendanceMachineManager)
+        {
+            _contextAccessor = contextAccessor;
+            _attendanceMachineManager = attendanceMachineManager;
+        }
+        // GET: AttendanceMachinesController
+        public async Task<ActionResult> Index()
+        {
+            var allAttendance = await _attendanceMachineManager.GetAllAsync();
+            return View(allAttendance);
+        }
+
+        // GET: AttendanceMachinesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AttendanceMachinesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AttendanceMachinesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AttendanceMachinesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AttendanceMachinesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AttendanceMachinesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AttendanceMachinesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

bad24c76d868aa3c210151144e8858589aaec2be
golam habib palash <golamhabibpalash@hotmail.com>
2022-06-17 16:32:25 +0600

    Questions Controller Added and linked

---
+++ SMS.App/Controllers/QuestionBanksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SMS.App.Controllers
+{
+    public class QuestionBanksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

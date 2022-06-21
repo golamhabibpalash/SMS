5c64cc09bf0bf85246f6a08eaa6372f82a265cb3
golam habib palash <golamhabibpalash@hotmail.com>
2022-06-22 00:51:38 +0600

    Chapter unit of work done

---
+++ SMS.BLL/Managers/Chapter.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Managers.Base;
+using SMS.BLL.Contracts;
+using SMS.DAL.Contracts;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.BLL.Managers
+{
+    internal class ChapterManager : Manager<Chapter>, IChapterManager
+    {
+        private readonly IChapterRepository _repository;
+        public ChapterManager(IChapterRepository repository) : base(repository)
+        {
+            _repository = repository;
+        }
+    }
+}

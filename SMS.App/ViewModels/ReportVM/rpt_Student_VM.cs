58ecd97fe444c514e4f8f9dd1be11d1cd3f97b46
golam habib palash <golamhabibpalash@hotmail.com>
2023-03-03 21:06:58 +0600

    report related work

---
+++ SMS.App/ViewModels/ReportVM/rpt_Student_VM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS.App.ViewModels.ReportVM
+{
+    public class rpt_Student_VM
+    {
+        [Display(Name="Academic Session")]
+        public int AcademicSessionId { get; set; }
+
+        [Display(Name = "Academic Class")] 
+        public int AcademicClassId { get; set; }
+
+        [Display(Name = "Academic Section")] 
+        public int AcademicSectionId { get; set; }
+    }
+}

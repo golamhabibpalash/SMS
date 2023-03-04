c508a84a878957c321b9ddb4cdae6c98b6826c4e
golam habib palash <golamhabibpalash@hotmail.com>
2023-03-04 08:45:25 +0600

    Academic Exam Layer Created

---
+++ SMS.BLL/Managers/AcademicExamManager.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Managers.Base;
+using SMS.BLL.Contracts;
+using SMS.DAL.Contracts;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.BLL.Managers
+{
+    public class AcademicExamManager:Manager<AcademicExam>, IAcademicExamManager
+    {
+        private readonly IAcademicExamRepository _academicExamRepository;
+        public AcademicExamManager(IAcademicExamRepository academicExamRepository):base(academicExamRepository)
+        {
+            _academicExamRepository = academicExamRepository;
+        }
+    }
+}
---
+++ SMS.DAL/Repositories/AcademicExamRepository.cs
@@ -0,0 +1,21 @@
+﻿using SMS.DAL.Contracts;
+using SMS.DAL.Repositories.Base;
+using SMS.DB;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.DAL.Repositories
+{
+    public class AcademicExamRepository : Repository<AcademicExam>, IAcademicExamRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public AcademicExamRepository(ApplicationDbContext context):base(context)
+        {
+            
+        }
+    }
+}

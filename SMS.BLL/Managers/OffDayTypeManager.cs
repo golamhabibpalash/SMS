c6cb4674666e1062a479f803a5251697dd6cda53
golam habib palash <golamhabibpalash@hotmail.com>
2023-03-06 22:45:13 +0600

    layer developed for OffDayType

---
+++ SMS.BLL/Managers/OffDayTypeManager.cs
@@ -0,0 +1,21 @@
+﻿using BLL.Managers.Base;
+using SMS.BLL.Contracts;
+using SMS.DAL.Contracts;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.BLL.Managers
+{
+    public class OffDayTypeManager:Manager<OffDayType>,IOffDayTypeManager
+    {
+        private readonly IOffDayTypeRepository _offDayTypeRepository;
+        public OffDayTypeManager(IOffDayTypeRepository offDayTypeRepository):base(offDayTypeRepository)
+        {
+            
+        }
+    }
+}
---
+++ SMS.DAL/Repositories/OffDayTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using SMS.DAL.Repositories.Base;
+using SMS.DB;
+using SMS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.DAL.Repositories
+{
+    public class OffDayTypeRepository:Repository<OffDayType>,IOffDayTypeRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public OffDayTypeRepository(ApplicationDbContext context):base(context)
+        {
+            
+        }
+    }
+}

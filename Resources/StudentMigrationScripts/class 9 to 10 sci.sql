63268c59fa1b1837b1a66381be90619d1a5d8ddb
golamhabibpalash <golamhabibpalash@gmail.com>
2025-01-03 22:10:44 +0600

    Some Script added regarding student migration

---
+++ Resources/StudentMigrationScripts/class 9 to 10 sci.sql
@@ -0,0 +1,87 @@
+
+-- Class Nine to Ten Science
+DECLARE @NewAcademicClassId INT = 5;
+DECLARE @OldAcademicClassId INT = 8;
+DECLARE @OldAcademicSessionId INT = 4;
+DECLARE @NewAcademicSessionId INT = 5;
+DECLARE @NewRollPrefix VARCHAR(10)= '2510';
+DECLARE @AcademicExamGroupId INT = 32;
+update 
+  s 
+set 
+  ClassRoll = 0 
+from 
+  student s 
+where 
+  s.AcademicClassId = @NewAcademicClassId 
+  and s.AcademicSessionId = @NewAcademicSessionId;
+update 
+  s 
+set 
+  s.ClassRoll = @NewRollPrefix + RIGHT(
+    '000' + CAST(r.Rank as varchar), 
+    3
+  ), 
+  s.AcademicClassId = @NewAcademicClassId, 
+  s.AcademicSessionId = @NewAcademicSessionId 
+from 
+  Student as s 
+  join (
+    select 
+      t.* 
+    from 
+      ExamResults t 
+    where 
+      t.AcademicClassId = @OldAcademicClassId 
+      and t.AcademicExamGroupId = @AcademicExamGroupId
+  ) as r on s.Id = r.StudentId;
+update 
+  s 
+set 
+  s.Status = 0, 
+  s.SMSService = 0 
+from 
+  Student s 
+where 
+  s.AcademicClassId = @OldAcademicClassId 
+  and s.AcademicSessionId = @OldAcademicSessionId;
+DECLARE @LastClassRoll int;
+SELECT 
+  @LastClassRoll = MAX(s.ClassRoll) 
+from 
+  Student s 
+where 
+  s.AcademicClassId = @NewAcademicClassId 
+  and s.AcademicSessionId = @NewAcademicSessionId;
+WITH OrderedStudents AS (
+  SELECT 
+    Id, 
+    ROW_NUMBER() OVER (
+      ORDER BY 
+        Id
+    ) AS RowNum 
+  FROM 
+    Student 
+  WHERE 
+    ClassRoll = 0
+)
+UPDATE 
+  s 
+SET 
+  s.ClassRoll = CAST(@LastClassRoll + RowNum AS VARCHAR) 
+FROM 
+  Student AS s 
+  JOIN OrderedStudents AS o ON s.Id = o.Id;
+
+
+select 
+  t.UniqueId, 
+  t.ClassRoll, 
+  t.* 
+from 
+  student t 
+where 
+  t.AcademicClassId = @NewAcademicClassId 
+  and t.AcademicSessionId = @NewAcademicSessionId
+order by 
+  t.ClassRoll;

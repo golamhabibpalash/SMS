49d56e85314f0305c7ef5f93c4c6f62652358ed8
golamhabibpalash <golamhabibpalash@gmail.com>
2025-02-22 07:35:07 +0600

    Attendance related change in hangfire

--- SMS.App/Controllers/HangfireController.cs
+++ SMS.App/Controllers/HangfireController.cs
@@ -239,7 +239,7 @@ namespace SMS.App.Controllers
                     {
                         foreach (Tran_MachineRawPunch attendance in todaysAllCheckInAttendance)
                         {
-                            Student student = await _studentManager.GetStudentByUniqueIdAsync(Convert.ToInt32(attendance.CardNo.Trim()).ToString());
+                            Student student = await _studentManager.GetStudentByUniqueIdAsync(attendance.CardNo.Trim());
 
                             if (student == null || student.GenderId != 1 || student.Status == false || student.SMSService == false)
                             {
@@ -321,7 +321,7 @@ namespace SMS.App.Controllers
                     {
                         foreach (Tran_MachineRawPunch attendance in todaysAllCheckInAttendance)
                         {
-                            Student student = await _studentManager.GetStudentByUniqueIdAsync(Convert.ToInt32(attendance.CardNo.Trim()).ToString());
+                            Student student = await _studentManager.GetStudentByUniqueIdAsync(attendance.CardNo.Trim());
 
                             if (student == null || student.GenderId != 2 || student.Status == false || student.SMSService != true)
                             {

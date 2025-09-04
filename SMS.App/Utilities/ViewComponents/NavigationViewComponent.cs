9bb52a6a095a165554f548e8d194592e0175f97d
golamhabibpalash <golamhabibpalash@gmail.com>
2025-09-04 07:52:36 +0600

    Navigation modified

--- SMS.App/Utilities/ViewComponents/NavigationViewComponent.cs
+++ SMS.App/Utilities/ViewComponents/NavigationViewComponent.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using SMS.App.ViewModels.ModuleSubModuleVM;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SMS.App.Utilities.ViewComponents;
 
@@ -18,6 +20,10 @@ public class NavigationViewComponent : ViewComponent
 
     public IViewComponentResult Invoke()
     {
+        var user = _httpContextAccessor.HttpContext?.User;
+        if (user == null) return View(new SiteMap { Modules = new List<Module>() });
+
+        // Get current route for active highlighting
         var routeData = _httpContextAccessor.HttpContext?.GetRouteData();
         var currentController = routeData?.Values["controller"]?.ToString();
         var currentAction = routeData?.Values["action"]?.ToString();
@@ -25,6 +31,29 @@ public class NavigationViewComponent : ViewComponent
         ViewData["CurrentController"] = currentController;
         ViewData["CurrentAction"] = currentAction;
 
-        return View(_siteMap);
+        // Filter modules based on Permission
+        var filteredModules = _siteMap.Modules
+            .Where(m => string.IsNullOrEmpty(m.Permission) || user.HasClaim("Permission", m.Permission))
+            .Select(m => new Module
+            {
+                SystemName = m.SystemName,
+                DisplayName = m.DisplayName,
+                Icon = m.Icon,
+                Permission = m.Permission,
+                Submodules = m.Submodules.Select(s => new Submodule
+                {
+                    SystemName = s.SystemName,
+                    DisplayName = s.DisplayName,
+                    Icon = s.Icon,
+                    Items = s.Items
+                        .Where(i => string.IsNullOrEmpty(i.Claim) || user.HasClaim("Permission", i.Claim))
+                        .ToList()
+                }).ToList()
+            })
+            .ToList();
+
+        var filteredSiteMap = new SiteMap { Modules = filteredModules };
+
+        return View(filteredSiteMap);
     }
 }

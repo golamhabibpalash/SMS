cb835ba91121a1299df86e9b282b55ac58059b2e
golamhabibpalash <golamhabibpalash@gmail.com>
2025-09-15 07:41:26 +0600

    New Theme (Smart) installed

---
+++ SMS.App/Utilities/Others/AppColors.cs
@@ -0,0 +1,7 @@
+﻿namespace SMS.App.Utilities.Others;
+
+public static class AppColors
+{
+    public static string TopNav = "#2C456B";
+    public static string TableTitleBackground = "#83AFF0";
+}
---
+++ SMS.App/ViewModels/Layout/LayoutHeaderVM.cs
@@ -0,0 +1,6 @@
+﻿namespace SMS.App.ViewModels.Layout;
+
+public class LayoutHeaderVM
+{
+    public string InstituteName { get; set; }
+}

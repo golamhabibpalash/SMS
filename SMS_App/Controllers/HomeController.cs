539083073bf5392b819be22e80583c98c6efd6dc
golamhabibpalash <golamhabibpalash@gmail.com>
2026-01-07 07:48:00 +0600

    remove warning now

--- SMS_App/Controllers/HomeController.cs
+++ SMS_App/Controllers/HomeController.cs
@@ -51,123 +51,123 @@ namespace SMS_App.Controllers
         //    _userManager = userManager;
         //    _instituteManager = instituteManager;
         //}
-        //public async Task<IActionResult> Index()
-        //{
-        //    HttpContext.Session.SetString("macAddress", MACService.GetMAC());
-        //    var user = await _userManager.GetUserAsync(User);
-        //    HttpContext.Session.SetString("UserId",user.Id);
-        //    Institute institute = await _instituteManager.GetFirstOrDefaultAsync();
-        //    ViewBag.InstituteLogo = institute.Logo;
-        //    ViewBag.InstituteName = institute.Name;
-        //    return View();
-        //}
         public async Task<IActionResult> Index()
         {
             HttpContext.Session.SetString("macAddress", MACService.GetMAC());
-
-            Institute institute = new Institute();
-            var allInfo = await _instituteManager.GetAllAsync();
-            if (allInfo.Count() > 0)
-            {
-                institute = allInfo.FirstOrDefault();
-                ViewBag.InstituteName = institute.Name;
-                ViewBag.InstituteLogo = institute.Logo;
-            }
             var user = await _userManager.GetUserAsync(User);
             HttpContext.Session.SetString("UserId", user.Id);
-
-            DashboardIndexVM DashboardVM = new DashboardIndexVM();
-            IReadOnlyCollection<Student> students = await _studentManager.GetAllAsync();
-            students = students.Where(s => s.Status == true).ToList();
-
-            DashboardVM.Students = (ICollection<Student>)students;
-            DashboardVM.Employees = (ICollection<Employee>)await _employeeManager.GetAllAsync();
-
-            DashboardVM.Employees = DashboardVM.Employees.Where(s => s.Status == true).ToList();
-
-            var todaysAllAttendance = await _attendanceMachineManager.GetAllAttendanceByDateAsync(DateTime.Now.Date);
-            var todaysAllUniqeAttendance = todaysAllAttendance.GroupBy(a => a.CardNo).ToList();
-            var allDesignations = await _designationManager.GetAllAsync();
-            allDesignations = allDesignations.Where(d => d.Employees.Count() > 0).ToList();
-
-            List<TodaysAttendanceEmpVM> todaysAttendanceEmpVMs = new List<TodaysAttendanceEmpVM>();
-            foreach (var designation in allDesignations)
-            {
-                TodaysAttendanceEmpVM todaysAttendanceEmpVM = new TodaysAttendanceEmpVM();
-                todaysAttendanceEmpVM.Designation = designation;
-                todaysAttendanceEmpVM.AttendedEmployees = (from e in designation.Employees
-                                                           from a in todaysAllUniqeAttendance
-                                                           where a.Key == e.Phone.Substring(e.Phone.Length - 9)
-                                                           && e.Status == true
-                                                           select e).ToList();
-                todaysAttendanceEmpVM.TotalEmployee = designation.Employees.Where(e => e.Status == true).Count();
-                todaysAttendanceEmpVMs.Add(todaysAttendanceEmpVM);
-            }
-            DashboardVM.TodaysAttendanceEmpVMs = todaysAttendanceEmpVMs;
-            var allAcademicClass = await _academicClassManager.GetAllAsync();
-
-            List<TodaysAttendanceStuVM> todaysAttendanceStuVMs = new List<TodaysAttendanceStuVM>();
-            foreach (var aClass in allAcademicClass.Where(c => c.Students.Count() > 0))
-            {
-                TodaysAttendanceStuVM todaysAttendanceStuVM = new TodaysAttendanceStuVM();
-                todaysAttendanceStuVM.AcademicClass = aClass;
-                todaysAttendanceStuVM.AttendedStudents = (from s in aClass.Students
-                                                          from a in todaysAllUniqeAttendance
-                                                          where a.Key == s.ClassRoll.ToString().PadLeft(8, '0')
-                                                          && s.Status == true
-                                                          select s).ToList();
-                todaysAttendanceStuVM.TotalStudent = aClass.Students.Count();
-
-                todaysAttendanceStuVMs.Add(todaysAttendanceStuVM);
-            }
-            DashboardVM.TodaysAttendanceStuVMs = todaysAttendanceStuVMs;
-
-            //Daily Collection Start here=====================
-            List<PaymentCollection> dPaymentCollections = new List<PaymentCollection>();
-            try
-            {
-                List<StudentPaymentSummeryVM> dailyPaymentSummery = (List<StudentPaymentSummeryVM>)await _studentPaymentManager.GetPaymentSummeryByDate(DateTime.Now.ToString("ddMMyyyy"));
-                foreach (var item in dailyPaymentSummery)
-                {
-                    PaymentCollection dPC = new PaymentCollection();
-                    dPC.AcademicClassName = item.AcademicClassName;
-                    dPC.Amount = item.Payments;
-
-                    dPaymentCollections.Add(dPC);
-                }
-
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-            DashboardVM.DailyCollections = dPaymentCollections;
-
-
-            //Monthly Collection Start here ===================
-            List<PaymentCollection> mPaymentCollections = new List<PaymentCollection>();
-            try
-            {
-                List<StudentPaymentSummeryVM> monthlyPaymentSummery = (List<StudentPaymentSummeryVM>)await _studentPaymentManager.GetPaymentSummeryByMonthYear(DateTime.Now.ToString("MMyyyy"));
-                foreach (var item in monthlyPaymentSummery)
-                {
-                    PaymentCollection dPC = new PaymentCollection();
-                    dPC.AcademicClassName = item.AcademicClassName;
-                    dPC.Amount = item.Payments;
-                    mPaymentCollections.Add(dPC);
-                }
-
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-            DashboardVM.MonthlyCollections = mPaymentCollections;
-
-            return View(DashboardVM);
+            Institute institute = await _instituteManager.GetFirstOrDefaultAsync();
+            ViewBag.InstituteLogo = institute.Logo;
+            ViewBag.InstituteName = institute.Name;
+            return View();
         }
+        //public async Task<IActionResult> Index()
+        //{
+        //    HttpContext.Session.SetString("macAddress", MACService.GetMAC());
+
+        //    Institute institute = new Institute();
+        //    var allInfo = await _instituteManager.GetAllAsync();
+        //    if (allInfo.Count() > 0)
+        //    {
+        //        institute = allInfo.FirstOrDefault();
+        //        ViewBag.InstituteName = institute.Name;
+        //        ViewBag.InstituteLogo = institute.Logo;
+        //    }
+        //    var user = await _userManager.GetUserAsync(User);
+        //    HttpContext.Session.SetString("UserId", user.Id);
+
+        //    DashboardIndexVM DashboardVM = new DashboardIndexVM();
+        //    IReadOnlyCollection<Student> students = await _studentManager.GetAllAsync();
+        //    students = students.Where(s => s.Status == true).ToList();
+
+        //    DashboardVM.Students = (ICollection<Student>)students;
+        //    DashboardVM.Employees = (ICollection<Employee>)await _employeeManager.GetAllAsync();
+
+        //    DashboardVM.Employees = DashboardVM.Employees.Where(s => s.Status == true).ToList();
+
+        //    var todaysAllAttendance = await _attendanceMachineManager.GetAllAttendanceByDateAsync(DateTime.Now.Date);
+        //    var todaysAllUniqeAttendance = todaysAllAttendance.GroupBy(a => a.CardNo).ToList();
+        //    var allDesignations = await _designationManager.GetAllAsync();
+        //    allDesignations = allDesignations.Where(d => d.Employees.Count() > 0).ToList();
+
+        //    List<TodaysAttendanceEmpVM> todaysAttendanceEmpVMs = new List<TodaysAttendanceEmpVM>();
+        //    foreach (var designation in allDesignations)
+        //    {
+        //        TodaysAttendanceEmpVM todaysAttendanceEmpVM = new TodaysAttendanceEmpVM();
+        //        todaysAttendanceEmpVM.Designation = designation;
+        //        todaysAttendanceEmpVM.AttendedEmployees = (from e in designation.Employees
+        //                                                   from a in todaysAllUniqeAttendance
+        //                                                   where a.Key == e.Phone.Substring(e.Phone.Length - 9)
+        //                                                   && e.Status == true
+        //                                                   select e).ToList();
+        //        todaysAttendanceEmpVM.TotalEmployee = designation.Employees.Where(e => e.Status == true).Count();
+        //        todaysAttendanceEmpVMs.Add(todaysAttendanceEmpVM);
+        //    }
+        //    DashboardVM.TodaysAttendanceEmpVMs = todaysAttendanceEmpVMs;
+        //    var allAcademicClass = await _academicClassManager.GetAllAsync();
+
+        //    List<TodaysAttendanceStuVM> todaysAttendanceStuVMs = new List<TodaysAttendanceStuVM>();
+        //    foreach (var aClass in allAcademicClass.Where(c => c.Students.Count() > 0))
+        //    {
+        //        TodaysAttendanceStuVM todaysAttendanceStuVM = new TodaysAttendanceStuVM();
+        //        todaysAttendanceStuVM.AcademicClass = aClass;
+        //        todaysAttendanceStuVM.AttendedStudents = (from s in aClass.Students
+        //                                                  from a in todaysAllUniqeAttendance
+        //                                                  where a.Key == s.ClassRoll.ToString().PadLeft(8, '0')
+        //                                                  && s.Status == true
+        //                                                  select s).ToList();
+        //        todaysAttendanceStuVM.TotalStudent = aClass.Students.Count();
+
+        //        todaysAttendanceStuVMs.Add(todaysAttendanceStuVM);
+        //    }
+        //    DashboardVM.TodaysAttendanceStuVMs = todaysAttendanceStuVMs;
+
+        //    //Daily Collection Start here=====================
+        //    List<PaymentCollection> dPaymentCollections = new List<PaymentCollection>();
+        //    try
+        //    {
+        //        List<StudentPaymentSummeryVM> dailyPaymentSummery = (List<StudentPaymentSummeryVM>)await _studentPaymentManager.GetPaymentSummeryByDate(DateTime.Now.ToString("ddMMyyyy"));
+        //        foreach (var item in dailyPaymentSummery)
+        //        {
+        //            PaymentCollection dPC = new PaymentCollection();
+        //            dPC.AcademicClassName = item.AcademicClassName;
+        //            dPC.Amount = item.Payments;
+
+        //            dPaymentCollections.Add(dPC);
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //    DashboardVM.DailyCollections = dPaymentCollections;
+
+
+        //    //Monthly Collection Start here ===================
+        //    List<PaymentCollection> mPaymentCollections = new List<PaymentCollection>();
+        //    try
+        //    {
+        //        List<StudentPaymentSummeryVM> monthlyPaymentSummery = (List<StudentPaymentSummeryVM>)await _studentPaymentManager.GetPaymentSummeryByMonthYear(DateTime.Now.ToString("MMyyyy"));
+        //        foreach (var item in monthlyPaymentSummery)
+        //        {
+        //            PaymentCollection dPC = new PaymentCollection();
+        //            dPC.AcademicClassName = item.AcademicClassName;
+        //            dPC.Amount = item.Payments;
+        //            mPaymentCollections.Add(dPC);
+        //        }
+
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+        //    DashboardVM.MonthlyCollections = mPaymentCollections;
+
+        //    return View(DashboardVM);
+        //}
 
         public IActionResult Privacy()
         {

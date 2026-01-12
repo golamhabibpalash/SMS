405e2aa53cde503caea66357f0fa67a1a98fb1ea
g.habib <g.habib@blmanagedservices.com>
2026-01-12 17:01:50 +0600

    Employee Nominee Phone number modified

--- SMS_App/ViewModels/Employees/EmployeeEditVM.cs
+++ SMS_App/ViewModels/Employees/EmployeeEditVM.cs
@@ -46,7 +46,7 @@ public class EmployeeEditVM
     [Display(Name = "Nominee Name")]
     public string? Nominee { get; set; }
 
-    [Range(01300000000, 01999999999)]
+    [RegularExpression(@"^(01[3-9]\d{8})?$", ErrorMessage = "Enter a valid phone number.")]
     public string? NomineePhone { get; set; }
 
     [Display(Name = "Employee Type")]
